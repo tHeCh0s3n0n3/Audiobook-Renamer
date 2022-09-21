8b8fe7cff2b7b48ad5aec2c3ea07c6274dab2f13
Tarek Fadel <dev@tarekfadel.com>
2022-09-21 19:13:06 +0400

    Switch to using TagLibSharp in test app.

--- Test App/MainForm.cs
+++ Test App/MainForm.cs
@@ -1,4 +1,4 @@
-using ID3Helper;
+using System.Text;
 
 namespace Test_App;
 
@@ -13,7 +13,7 @@ public partial class MainForm : Form
     {
         using OpenFileDialog ofd = new()
         {
-            Filter = "MP3 files (*.mp3)|*.mp3"
+            Filter = "MP3 files (*.mp3)|*.mp3|All Files (*.*)|*.*"
             , Multiselect = false
             , Title = "Select MP3 file"
         };
@@ -30,16 +30,74 @@ public partial class MainForm : Form
         txtBookNumber.Clear();
         txtJSON.Clear();
 
-        Audiobook? audiobook = ID3Helper.Helper.ParseID3Tags(ofd.FileName);
-        if (audiobook is null)
-        {
-            return;
-        }
+        //Audiobook? audiobook = ID3Helper.Helper.ParseID3Tags(ofd.FileName);
+        //if (audiobook is null)
+        //{
+        //    return;
+        //}
 
-        txtTitle.Text = audiobook.Title;
-        txtAuthor.Text = audiobook.Author;
-        txtSeriesName.Text = audiobook.Series;
-        txtBookNumber.Text = audiobook.BookNumber;
-        txtJSON.Text = audiobook.JSONRepresentation;
+        //txtTitle.Text = audiobook.Title;
+        //txtAuthor.Text = audiobook.Author;
+        //txtSeriesName.Text = audiobook.Series;
+        //txtBookNumber.Text = audiobook.BookNumber;
+        //txtJSON.Text = audiobook.JSONRepresentation;
+
+        TagLib.File tagFile = TagLib.File.Create(ofd.FileName);
+
+        txtTitle.Text = tagFile.Tag.Title;
+        foreach(TagLib.Tag tagContainer in (tagFile.Tag as TagLib.NonContainer.Tag)?.Tags ?? Array.Empty<TagLib.Tag>())
+        {
+            if (tagContainer.TagTypes == TagLib.TagTypes.Id3v2)
+            {
+                if (tagContainer is not TagLib.Id3v2.Tag id3Tags)
+                {
+                    continue;
+                }
+                foreach (var id3Tag in id3Tags)
+                {
+                    var tif = id3Tag as TagLib.Id3v2.UserTextInformationFrame;
+                    if (tif is not null)
+                    {
+                        string? desc = tif.Description;
+                        string[] tift = tif.Text;
+                        string? tiftStr = string.Join(", ", tift);
+                        if (desc is not null
+                            && tiftStr is not null)
+                        {
+                            if (desc.ToUpper().Equals("AUTHOR"))
+                            {
+                                txtAuthor.Text = tiftStr;
+                            }
+                            if (desc.ToUpper().Equals("SERIES"))
+                            {
+                                txtSeriesName.Text = tiftStr;
+                            }
+                            if (desc.ToUpper().Equals("SERIES-PART"))
+                            {
+                                txtBookNumber.Text = tiftStr;
+                            }
+                            if (desc.ToUpper().Equals("JSON64"))
+                            {
+                                int encoding = tif.TextEncoding switch
+                                {
+                                    TagLib.StringType.Latin1 => 28591,
+                                    TagLib.StringType.UTF16 => 1200,
+                                    TagLib.StringType.UTF16LE => 1200,
+                                    TagLib.StringType.UTF16BE => 1201,
+                                    TagLib.StringType.UTF8 => 65001,
+                                    _ => 65001
+                                };
+                                txtJSON.Text = Encoding.GetEncoding(encoding)
+                                                       .GetString(Convert.FromBase64String(tiftStr));
+                            }
+                        }
+
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+                        string result = $"[{desc}] {tiftStr}";
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+                    }
+                }
+            }
+        }
     }
 }
